03ceb41cbdc326fd79037be1e0025975889578f1
chewear <myron.reyes37@gmail.com>
2024-05-27 20:09:53 +0800

    add nav functions

--- echoesofthepast/index.js
+++ echoesofthepast/index.js
@@ -37,3 +37,21 @@ function onDownloadClick() {
 //     event.preventDefault();
 //     window.location.href = 'download/EOTPv1.4.zip';  
 // });
+
+
+function scrollToDownload(){
+    document.getElementById('downloader').scrollIntoView({ behavior: 'smooth' });
+}
+
+function sc1(){
+    document.getElementById('c1').scrollIntoView({ behavior: 'smooth' });
+}
+function sc2(){
+    document.getElementById('c2').scrollIntoView({ behavior: 'smooth' });
+}
+function sc3(){
+    document.getElementById('c3').scrollIntoView({ behavior: 'smooth' });
+}
+function sc4(){
+    document.getElementById('c4').scrollIntoView({ behavior: 'smooth' });
+}
